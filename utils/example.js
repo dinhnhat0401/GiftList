351a0d283d308a6b78a57b3cf3d2e70aa28b91e7
dinhnhat0401 <nhatphutho@gmail.com>
2024-07-01 09:09:17 +0900

    finished

--- utils/example.js
+++ utils/example.js
@@ -1,6 +1,6 @@
-const MerkleTree = require('./MerkleTree');
-const niceList = require('./niceList');
-const verifyProof = require('./verifyProof');
+const MerkleTree = require("./MerkleTree");
+const niceList = require("./niceList");
+const verifyProof = require("./verifyProof");
 
 // create the merkle tree for the whole nice list
 const merkleTree = new MerkleTree(niceList);
@@ -8,12 +8,18 @@ const merkleTree = new MerkleTree(niceList);
 // get the root
 const root = merkleTree.getRoot();
 
-// find the proof that norman block is in the list 
-const name = 'Norman Block';
-const index = niceList.findIndex(n => n === name);
+console.log("root = " + root);
+
+// find the proof that norman block is in the list
+const name = "Norman Block";
+const index = niceList.findIndex((n) => n === name);
 const proof = merkleTree.getProof(index);
 
 // verify proof against the Merkle Root
-console.log( verifyProof(proof, name, root) ); // true, Norman Block is in the list!
+console.log(verifyProof(proof, name, root)); // true, Norman Block is in the list!
 
 // TRY IT OUT: what happens if you try a name not in the list, or a fake proof?
+const name2 = "Norman Block 1";
+const index2 = niceList.findIndex((n) => n === name);
+const proof2 = merkleTree.getProof(index2);
+console.log(verifyProof(proof2, name2, root));
